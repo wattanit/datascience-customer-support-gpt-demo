f1c68700266290ae0f806a36706b82d8c3aa2410
Wattanit Hotrakool <wattanit.ho@tnds.com>
2024-01-31 12:33:29 +0700

    Create basic Actix API scaffold

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/threads")]
+async fn get_threads() -> impl Responder {
+    HttpResponse::Ok().body("Get threads list")
+}
+
+#[get("/threads/{id}")]
+async fn get_thread(id: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("Get thread {}", id))
+}
+
+#[post("/threads")]
+async fn create_thread(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body("Create thread")
+}
+
+#[post("/threads/{id}/message")]
+async fn create_message(id: String, req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("Create message in thread {}", id))
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("MyMo Customer Service Demo API")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://127.0.0.1:8080/");
+    HttpServer::new(|| {
+        App::new()
+            .service(get_threads)
+            .service(get_thread)
+            .service(create_thread)
+            .service(create_message)
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
